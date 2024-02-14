1974d9567ddce796bc4e2a0b6373b3434d0a9574
omeror <118984867+omror@users.noreply.github.com>
2024-02-14 20:23:14 +0300

    Create select_random_friend.py

---
+++ select_random_friend.py
@@ -0,0 +1,18 @@
+import random
+
+friends = [
+    'Olek',
+    'Carmela',
+    'Laura',
+    'Megan',
+    'Keith',
+    'Anna',
+    'Skyler',
+    'Amy',
+    'Nadya'
+]
+
+selected = random.choice(friends)
+
+print('Who should I facetime today?')
+print(selected)
